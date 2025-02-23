2bf76563261fc84c16c07ee02891000620e46719
Manjeeth Shenoy <mamsh@eg.dk>
2025-02-23 20:51:02 +0530

    added links to social media
    added links to social media

--- src/app/page.js
+++ src/app/page.js
@@ -21,8 +21,8 @@ export default async function Home() {
     <main className="bg-white dark:bg-gray-900">
       <Header />
       <div className="relative isolate flex flex-col sm:flex-row items-center gap-x-6 overflow-hidden bg-teal-500 px-4 py-3 sm:px-6 sm:py-2.5 mt-16">
-        <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-          <p className="text-sm leading-6 text-white text-center sm:text-left w-full sm:w-auto">
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-2 w-full justify-center">
+          <p className="text-base sm:text-lg leading-6 text-white text-center w-full">
             <strong className="font-semibold">Lifetime Member</strong>
             <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
               <circle r={1} cx={1} cy={1} />
